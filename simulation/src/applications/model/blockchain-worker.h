9ce13b47a203c925c835590f6c8138d3d265895d
uniform64 <jiny2048@gmail.com>
2022-11-08 21:58:49 +0800

    save

---
+++ simulation/src/applications/model/blockchain-worker.h
@@ -0,0 +1,47 @@
+#ifndef BLOCKCHAIN_WORKER
+#define BLOCKCHAIN_WORKER
+
+#include "ns3/application.h"
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+#include "ns3/ipv4-address.h"
+
+namespace ns3 {
+
+class Socket;
+class Packet;
+
+class BlockchainWorker : public Application
+{
+public:
+  static TypeId GetTypeId (void);
+
+  BlockchainWorker ();
+  virtual ~BlockchainWorker ();
+
+  void Init();
+  void Start();
+  void Stop();
+  void CommitBlock();
+
+protected:
+  virtual void DoDispose (void);
+
+private:
+  
+    virtual void StartApplication (void);
+    virtual void StopApplication (void);
+  
+    uint64_t m_nodeId;
+    uint16_t m_;
+  
+    Ipv4Address m_sip, m_dip;
+    uint16_t m_sport, m_dport;
+    uint32_t m_win; // bound of on-the-fly packets
+    uint64_t m_baseRtt; // base Rtt
+};
+  
+} // Namespace ns3
+
+
+#endif
